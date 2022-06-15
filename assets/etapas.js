95f8f52754288168dfb4ec6889709ca3c28b24ac
Lucas Del Bosco <lucasbfdb@id.uff.br>
2022-06-15 17:10:24 -0300

    Urna Pronta

---
+++ assets/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+   {
+      titulo: 'VEREADOR',
+      numeros: 5,
+      candidatos: [
+         {
+            numero: '12345',
+            nome: 'Rei Julian',
+            partido: 'MDGC',
+            fotos: [
+               {url:'Julian.jpg', legenda: 'Vereador'}
+            ]
+         },
+         {
+            numero: '54321',
+            nome: 'Bruno Madrigal',
+            partido: 'MADRG',
+            fotos: [
+               {url: 'Bruno.webp', legenda: 'Verador'}
+            ]
+         }
+      ]
+   },
+   {
+      titulo: 'PREFEITO',
+      numeros: 2,
+      candidatos: [
+         {
+            numero: '12',
+            nome: 'Edna Moda',
+            partido: 'INC',
+            vice: 'Violeta',
+            fotos: [
+               {url: 'Edna.webp', legenda: 'Prefeito'},
+               {url: 'Violeta.webp', legenda: 'Vice-Prefeito', small: true}
+            ]
+         },
+         {
+            numero: '21',
+            nome: 'Elsa',
+            partido: 'LERIGO',
+            vice: 'Olaf',
+            fotos: [
+               {url: 'Elsa.jpg', legenda: 'Prefeito'},
+               {url: 'Olaf.jpg', legenda: 'Vice-Prefeito', small: true}
+            ]
+         },
+      ]
+   }
+]
